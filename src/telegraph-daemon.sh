5ba4bcab25576293d606e2baa4b3722d32c88bd8
Mihail Podivilov <mihail@podivilov.ru>
2019-01-09 19:56:37 +0300

    Create telegraph-daemon.sh

---
+++ src/telegraph-daemon.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#
+# Daemon for Telegraph
+# (c) 2019 Mihail Podivilov
+#
+# See copyright notice in LICENSE
+#
+
+# Waiting for a new Telegraph device
+# being presented in system
+while :
+do
+  # If new Telegraph device found, run /usr/bin/telegraph
+  if lsblk -r | grep -q "sd.*[1-9]"; then
+    # Wait for 500 ms
+    # for slow USB devices
+    sleep 0.5
+
+    # Run Telegraph
+    /usr/bin/telegraph
+  fi
+
+  # Waiting for Telegraph device
+  # to disappear from system
+  while :
+  do
+    if lsblk -r | grep -q "sd.*[1-9]"; then
+      :
+    else
+      break
+    fi
+
+    # Wait for 500 ms
+    # to decrease the CPU utilization
+    sleep 0.1
+  done
+
+  # Wait for 500 ms
+  # to decrease the CPU utilization
+  sleep 0.5
+done
